c08702bfcb48d2f34334d33559b5ae9fd380a355
Mubin99 <ehatesham.al.mubin99@gmail.com>
2018-07-09 05:25:06 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Login</title>
+	</head>
+	<body>
+		<h1 align="center">Login</h1>
+		<form align="center" action="Clogin.php" method="POST">
+			<p> Username: <input type="text" name="Uname"/></p>
+			<p> Password: <input type="Password" name="Upass"/> </p>
+			<table align="center">
+			
+			<tr>
+			<td><button  type="Submit" name="btn">Login</button> </td>
+
+			</tr>
+		</form>
+
+		<form align="center" action="reg.php" mehto="POST">
+
+			<button type="Submit" name="register"> Register</button>
+			<br>
+			<br>
+		</form>
+		
+	</body>
+</html>
